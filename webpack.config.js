e98bee8fadad716851f98afd7d6c21556ad12965
Philip10023 <philipormorgan@gmail.com>
2017-11-29 10:50:46 -0500

    added source map change for build in production. getting prod bundle size down.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,36 @@
 const path = require('path')
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ],
-      test: /\.s?css$/
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+  if (process.env.NODE_ENV === 'production') {
+    delete config.devtool;
+    var webpack = require('webpack');
+    config.plugins = [
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
+    ];
   }
-}
-if (process.env.NODE_ENV === 'production') {
-  delete config.devtool;
-  var webpack = require('webpack');
-  config.plugins = [
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
-  ];
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        test: /\.s?css$/
+      }]
+    },
+    devtool: env ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
+  }
 }
